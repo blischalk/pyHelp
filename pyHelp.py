0a6856f77c5f8d2d72acb4ef6502ecc97092ab9a
Brett Lischalk <brett@brettlischalk.com>
2018-03-19 17:11:40 -0500

    Adding descriptive comments

--- pyHelp.py
+++ pyHelp.py
@@ -1,3 +1,6 @@
+# Set a global variable that all table formatting will reference
+formatter_string = "{0:<4}"
+
 def getStart():
     start = int(input("Starting value:  "))
     return start
@@ -8,27 +11,38 @@ def getEnd():
 
 def mulTable(start, end):
     # Print top left cell
-    print "{0:<4}".format("X"),
+    # Look into format. This allows columns to be
+    # a specific width so output looks nice
+    # See: https://stackoverflow.com/questions/19103052/python-string-formatting-columns-in-line
+    # Notice the comma at the end of the line.
+    # This will continue to print the values on the same line
+    # until a print statement without a comma is encountered
+    # See: https://stackoverflow.com/questions/5598181/python-multiple-prints-on-the-same-line
+    print formatter_string.format("X"),
 
     # Print header row
+    # We increase end by 1 because the range() is not
+    # inclusive of the final value
+    # See https://docs.python.org/2.7/library/functions.html#range
     for h in range(start, end+1):
         if h != end:
-            print "{0:<4}".format(h),
+            print formatter_string.format(h),
         else:
-            print "{0:<4}".format(h)
+            print formatter_string.format(h)
 
     for i in range(start, end+1):
-        # Print starting value first
-        print "{0:<4}".format(i),
+        # Print starting value first to generate
+        # the left hand column with the operand
+        print formatter_string.format(i),
 
         for j in range(start, end+1):
             result = i*j
             # End of a row
             if j == end:
-                print "{0:<4}".format(result)
+                print formatter_string.format(result)
             # Non-ending row value
             else:
-                print "{0:<4}".format(result),
+                print formatter_string.format(result),
       
 def main():
   start = getStart()
